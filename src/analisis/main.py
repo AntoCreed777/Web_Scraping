a097f85c002d64071f8bc340aec82c6ccd2ed474
AntoCreed <abenavides@desem.cl>
2025-09-09 14:46:08 -0300

    refactor: Se movieron funciones del main de analisis a un archivo aparte

--- src/analisis/main.py
+++ src/analisis/main.py
@@ -1,59 +1,14 @@
 import os
 import sys
-from typing import Optional
 
 import matplotlib.pyplot as plt
 import pandas as pd
+from utilities import importar_data_frame, imprimir_data_frame, split_df
 
+# Para poder importar de src.scraping
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 
-from src.scraping.const import settings
 from src.scraping.datos_serie import SerieColumn, SerieNullValues
-
-
-def importar_data_frame() -> pd.DataFrame:
-    return pd.read_pickle(settings.nombre_archivo_pkl)
-
-
-def imprimir_data_frame(
-    df: pd.DataFrame,
-    mensaje: str,
-    columnas: Optional[list[str]] = None,
-    cantidad: Optional[int] = None,
-):
-    """
-    Imprime el DataFrame en formato tabla markdown.
-    Si se especifica una lista de columnas, solo muestra esas columnas.
-    """
-    if columnas is not None:
-        df = df[columnas]
-
-    print(f"\n{mensaje}")
-
-    if df.empty:
-        print("El DataFrame está vacío.")
-        return
-
-    if cantidad is None:
-        print(df.to_markdown(index=False))
-        print(f"Filas mostradas: {len(df)}")
-    else:
-        print(df.head(cantidad).to_markdown(index=False))
-
-
-def split_df(df: pd.DataFrame, columna: str) -> tuple[str, pd.DataFrame]:
-    COLUMNA_SPLIT = columna + "_split"
-
-    # Separar en listas
-    df[COLUMNA_SPLIT] = df[columna].str.split(",")
-
-    # Explode para tener una fila por cada elemento
-    df_exploded = df.explode(COLUMNA_SPLIT)
-
-    # Limpiar espacios extra
-    df_exploded[COLUMNA_SPLIT] = df_exploded[COLUMNA_SPLIT].str.strip()
-
-    return COLUMNA_SPLIT, df_exploded
 
 
 ###
